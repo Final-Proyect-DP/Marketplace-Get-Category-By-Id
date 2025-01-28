dc837b7a534b29bdec31b2553580714f5e38442e
Fernandonov21 <fernandonovillo21@gmail.com>
2025-01-28 14:40:14 -0500

    Add initial project structure with Flask setup, database configuration, and category retrieval API

---
+++ app/__init__.py
@@ -0,0 +1,21 @@
+from flask import Flask
+from .config.config import Config
+from .extensions.extensions import db
+
+from flasgger import Swagger
+from flask_cors import CORS
+from .controllers.category_controller import category_bp
+
+def create_app():
+    app = Flask(__name__)
+    app.config.from_object(Config)
+    
+    db.init_app(app)
+    Swagger(app, template_file="swagger_config.yaml")
+    CORS(app)  # Enable CORS for all routes
+
+    # Register Blueprints for the routes
+
+    app.register_blueprint(category_bp)
+
+    return app
---
+++ app/controllers/category_controller.py
@@ -0,0 +1,12 @@
+from flask import Blueprint, jsonify
+from app.models.models import Category
+
+category_bp = Blueprint('category', __name__)
+
+@category_bp.route('/categories/<int:id>', methods=['GET'])
+def get_category_by_id(id):
+    category = Category.query.get(id)
+    if not category:
+        return jsonify({"message": "Category not found"}), 404
+
+    return jsonify({"message": "Category fetched successfully", "category": category.to_dict()}), 200
